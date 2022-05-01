381236c0782d43d2c48f076a94d504a639295a31
Karol Leśniewski <lesniewski.karol99@gmail.com>
2022-05-01 20:23:44 +0200

    first mapping

--- src/main/java/com/webservices/calculator/lovecalculator/controller/LoveController.java
+++ src/main/java/com/webservices/calculator/lovecalculator/controller/LoveController.java
@@ -1,4 +1,21 @@
 package com.webservices.calculator.lovecalculator.controller;
 
+import com.webservices.calculator.lovecalculator.Love;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class LoveController {
+
+    @GetMapping("/calculate/first={first}&second={second}")
+    public Map<String,Object> calculateLove(@PathVariable String first, @PathVariable String second){
+        Map<String,Object> returnJSON = new HashMap<>();
+        returnJSON.put("names", new Love(first, second));
+        return returnJSON;
+    }
+
 }
